f1ee44976e9461d8ff259aab4cfe86fedcaa75f5
Yaview Lomelí <43951357-yaviewlomeli@users.noreply.replit.com>
2025-09-27 17:15:42 +0000

    .

--- client/src/pages/Servicios.tsx
+++ client/src/pages/Servicios.tsx
@@ -252,7 +252,7 @@ function Servicios() {
         appearance={appearance}
       >
         <Button size="lg" variant="secondary" asChild>
-          <Link href="/contact">Solicitar cotización</Link>
+          <Link href="/contacto">Solicitar cotización</Link>
         </Button>
       </HeroSection>
 
